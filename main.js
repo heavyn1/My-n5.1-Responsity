f569362bd2dfb49377b076d8c0351450b1614a6c
heavyn1 <bunedusupov968@gmail.com>
2024-05-23 13:27:17 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+const dasturchi = {
+    name : "Bunyodbek",
+    yosh : 25,
+    malumoti : "maktab"
+}
+
+const Maktab = {
+    numberStudents : 400,
+    teachers: 40,
+    rooms: 25,
+    restrooms: true,
+    gym: false,
+    furniture: ["chair" , "board" , "desk"]
+
+
+}
+
+console.log(Maktab.gym);
